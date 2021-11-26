babea55498fe2d07d8b46346b81908b11c937166
Shaimov87 <shaimov87@mail.ru>
2021-11-26 17:03:19 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] subjects = null;
+            int[][] marks = null;
+
+            Console.Write("\tPlease,enter number of subjects");
+            int n = Convert.ToInt32(Console.ReadLine());
+            subjects = new string[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"\tPlease,enter subject ({i+1}/{n}) ");
+                subjects[i] = Console.ReadLine();
+            }
+
+            marks = new int[n][];
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"\tPlease,enter number  marks for subject {subjects[i]} ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                marks[i] = new int[m];
+                for(int j = 0; j < m; j++)
+                {
+                    Console.Write($"\tPlease,enter marks ({i + 1}/{m}) ");
+                    marks[i][j] = Convert.ToInt32(Console.Read());
+                }
+                bool key=false;
+                marks[i].Average() > 2.5 && m > 2 ? key = true : key = false;            /*Console.WriteLine("\tCertified") : Console.WriteLine("\tNot Certified");*/
+            }
+
+            for(int i = 0; i < subjects.Length; i++)
+            {
+                Console.Write($"\t{subjects[i]}:\t\t ");
+                foreach (int mark in marks[i])
+                    Console.Write($"\t{mark} ");
+                Console.WriteLine();
+            }
+
+            double totalSum = 0;
+            for(int i = 0; i < subjects.Length; i++)
+            {
+                double av = marks[i].Average();
+                totalSum += av;
+                Console.WriteLine($"\tAverage mark of {subjects[i]} = {marks[i].Average()}");
+            }
+            Console.WriteLine($"\tAverage mark of study: {totalSum/subjects.Length}");
+        }
+    }
+}
